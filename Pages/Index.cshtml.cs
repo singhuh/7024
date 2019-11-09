29aae8f5d04890b0ad0cacc5e0f29208e132eb13
Umesh Singh <umesmail@gmail.com>
2019-11-09 04:03:10 -0500

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using QuickType;
+
+namespace XMLProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            using(var webClient = new WebClient()) 
+            {
+            string jsonString = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/v8yh-wpss.json");
+            var hospital = Hospital.FromJson(jsonString);
+            ViewData["Hospital"] = hospital;
+            }
+        }
+    }
+}
